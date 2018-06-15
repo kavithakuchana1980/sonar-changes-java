bcc8b233090898a4da7995cafcf245a8efe17ad4
kkkits <kbkitswgl@gmail.com>
2018-06-15 06:49:24 -0400

    initial step

---
+++ arthimetic.java
@@ -0,0 +1,20 @@
+public class arthimetic {
+
+	public static void main(String[] args) {
+		System.out.println(" The addition of two numbers");
+		int a=10,b=20,c=0;
+		System.out.print("The additionof two number is\n ");
+		c= a + b;
+		System.out.print(+c);
+		System.out.print("The subtraction two number is\n"); 
+		c=a-b;
+		System.out.println(+c);
+		System.out.print("The multiplication two number is\n ");
+		c=a * b;
+		System.out.print("The division two number is\n ");
+		System.out.println(+c);
+        c=a/b;
+        System.out.println(+c);
+	}
+
+}
